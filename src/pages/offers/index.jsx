1ab78a21aad0c7de9c73fb1cded1e1fd4c7bd240
amira-m-arafa <amira.arafa@getvoila.com>
2023-08-09 17:16:44 +0300

    enhace page loading

--- src/pages/offers/index.jsx
+++ src/pages/offers/index.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import ReactLoader from "../../Components/ReactLoader/ReactLoader";
 
@@ -5,4 +6,18 @@ const Offers = dynamic(() => import("../../Components/Offers"), {
   loading: () => <ReactLoader />,
 });
 
-export default Offers;
+const DelayedOffers = () => {
+  const [showOffers, setShowOffers] = useState(false);
+
+  useEffect(() => {
+    const delayTimeout = setTimeout(() => {
+      setShowOffers(true);
+    }, 500);
+
+    return () => clearTimeout(delayTimeout);
+  }, []);
+
+  return <>{showOffers ? <Offers /> : <ReactLoader />}</>;
+};
+
+export default DelayedOffers;
