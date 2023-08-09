32472dc2c2eae28bd6dc38a34d62186bcce64ada
amira-m-arafa <amira.arafa@getvoila.com>
2023-08-09 17:32:51 +0300

    tring suspense option

--- src/pages/offers/index.jsx
+++ src/pages/offers/index.jsx
@@ -1,23 +1,18 @@
-import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
+import React, { Suspense } from "react";
 import ReactLoader from "../../Components/ReactLoader/ReactLoader";
 
 const Offers = dynamic(() => import("../../Components/Offers"), {
   loading: () => <ReactLoader />,
+  suspense: true,
 });
 
 const DelayedOffers = () => {
-  const [showOffers, setShowOffers] = useState(false);
-
-  useEffect(() => {
-    const delayTimeout = setTimeout(() => {
-      setShowOffers(true);
-    }, 1000);
-
-    return () => clearTimeout(delayTimeout);
-  }, []);
-
-  return <>{showOffers ? <Offers /> : <ReactLoader />}</>;
+  return (
+    <Suspense fallback={<ReactLoader />}>
+      <Offers />
+    </Suspense>
+  );
 };
 
 export default DelayedOffers;
