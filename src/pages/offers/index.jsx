a7eda93598eb1d7a9c17903a07e4deb69739c3d7
amira-m-arafa <amira.arafa@getvoila.com>
2023-08-09 17:52:43 +0300

    test offers styles

--- src/pages/offers/index.jsx
+++ src/pages/offers/index.jsx
@@ -12,7 +12,7 @@ const DelayedOffers = () => {
   useEffect(() => {
     const delayTimeout = setTimeout(() => {
       setShowOffers(true);
-    }, 1500);
+    }, 1200);
 
     return () => clearTimeout(delayTimeout);
   }, []);
