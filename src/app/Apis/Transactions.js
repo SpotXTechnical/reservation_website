204077f176853bce21316493d293c2bde37c9318
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2024-04-18 12:05:14 +0200

    Add transaction details and wallet for user details page

--- src/app/Apis/Transactions.js
+++ src/app/Apis/Transactions.js
@@ -1,7 +1,29 @@
 import { axiosInstance } from "./AxiosInstance";
-export const getUserTransactions = async (id) => {
+export const getUserBalance = async () => {
   try {
-    const response = await axiosInstance.get(`/api/v1/users/${id}/wallet`);
+    const response = await axiosInstance.get(`/api/v1/user/wallet/balance`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const getUserTransactions = async () => {
+  try {
+    const response = await axiosInstance.get(
+      `/api/v1/user/wallet/transactions`
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const getTransaction = async (transactionId) => {
+  try {
+    const response = await axiosInstance.get(
+      `/api/v1/user/wallet/transactions/${transactionId}`
+    );
     return response.data;
   } catch (error) {
     throw new Error(error.message);
