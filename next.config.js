a2b36720ed6fbebacab817e1aec7ce7c0cba12f6
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2024-04-18 11:48:24 +0200

    Update next js to ver 13.4

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    reactStrictMode: false,
-  },
   i18n: {
     locales: ["en", "ar"],
     defaultLocale: "en",
   },
   cssModules: true,
-
 };
 
 module.exports = nextConfig;
