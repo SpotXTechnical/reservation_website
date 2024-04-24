17bd1820807e536e34570086da8069e9544f26c3
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2024-04-24 14:56:17 +0200

    Add company details to footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,21 +1,38 @@
 import { FormattedMessage } from "react-intl";
 import styles from "./Footer.module.css";
 import { useRouter } from "next/router";
-
+import CallUs from "../../../public/assets/callus.svg";
+import Mail from "../../../public/assets/mail.svg";
+import Address from "../../../public/assets/address.svg";
+import Image from "next/image";
 export default function Footer() {
   const router = useRouter();
   return (
     <div className={styles.footer_wrapper}>
       <div className="d-flex mb-4">
-        <div className="col-sm-6">
+        <address className="col-sm-6 d-flex flex-column">
           <p className={styles.about_spotx}>
-            <FormattedMessage id="home.mostPopularProperties" />
+            <FormattedMessage id="SpotX" />
           </p>
-          {/* <span className={styles.about}>
-            “Maecenas faucibus mollis interdum. Nullamion etaquis risus eget
-            urna mollis ornare vel eu leonardifaucibus mollis interdum”...{" "}
-          </span> */}
-        </div>
+          <p
+            className={`d-flex align-items-center gap-2 ${styles.contact_row}`}
+          >
+            <Image {...CallUs} alt="phone" />
+            <a href="tel:+201222381837">+201222381837</a>
+          </p>
+          <p
+            className={`d-flex align-items-center gap-2 ${styles.contact_row}`}
+          >
+            <Image {...Mail} alt="phone" />
+            <a href="mailto:info@spotx.app">info@spotx.app</a>
+          </p>
+          <p
+            className={`d-flex align-items-center gap-2 ${styles.contact_row}`}
+          >
+            <Image {...Address} alt="phone" />
+            الإسكندرية - قسم الرمل - ٦٩٦ طريق الحرية - لوران
+          </p>
+        </address>
         <div className="col-sm-3">
           <ul>
             {/* <li>
