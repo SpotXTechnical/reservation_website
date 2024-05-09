9b4cf302f22121347cc66982a7e8a8374949d87c
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2024-05-09 17:50:51 +0300

    Add base url based on environment variables

--- src/app/Apis/AxiosInstance.jsx
+++ src/app/Apis/AxiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import Router from "next/router";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://api-stage.spotx.app",
+  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}`,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
