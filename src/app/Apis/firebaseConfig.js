0454af0c0c4ca34228951118902928ac3c133abc
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2024-05-12 12:25:56 +0300

    Edit firebase config

--- src/app/Apis/firebaseConfig.js
+++ src/app/Apis/firebaseConfig.js
@@ -1,14 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, RecaptchaVerifier } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCReqZttDwMxHZp-qW2xkdeHQZIHU16Ez8",
-  authDomain: "spotx-otp.firebaseapp.com",
-  projectId: "spotx-otp",
-  storageBucket: "spotx-otp.appspot.com",
-  messagingSenderId: "487153413918",
-  appId: "1:487153413918:web:5f4d2491e012877d05e30f",
+  apiKey: "AIzaSyArEObhjKcua9LY-FPe4zqdDlJkV6zwABA",
+  authDomain: "spotx-5cafb.firebaseapp.com",
+  projectId: "spotx-5cafb",
+  storageBucket: "spotx-5cafb.appspot.com",
+  messagingSenderId: "850774658008",
+  appId: "1:850774658008:web:5c4016290e0ae31f4380ad",
+  measurementId: "G-KDD8KZ8XZD",
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+const analytics = getAnalytics(app);
