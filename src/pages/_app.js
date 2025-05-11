23ae3af3e25504d3adaf1d151a8f5a54fea7c841
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2025-05-11 22:54:29 +0300

    Add support for localization

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,39 +2,64 @@ import Layout from "../CusomLayout";
 import { IntlProvider } from "react-intl";
 import en from "../lang/en.json";
 import ar from "../lang/ar.json";
-import store from "../store/index";
-import { Provider } from "react-redux";
-import { useEffect, useState, useMemo } from "react";
+
+import { Provider, useSelector, useDispatch } from "react-redux";
 import { AuthInitializer } from "../Components/AuthInitializer/AuthInitializer";
 import BootStrapClient from "../Components/BootStrapClient/BootStrapClient";
+import { useEffect, useMemo } from "react";
+import { langAction } from "../store";
+import store from "../store";
 
-export default function MyApp({ Component, pageProps }) {
-  const [lang, setLang] = useState("en");
+function AppContent({ Component, pageProps }) {
+  const dispatch = useDispatch();
+  const { lang } = useSelector((state) => state.language);
   const getLayout = Component.getLayout;
 
   useEffect(() => {
-    if (localStorage.getItem("language")) {
-      setLang(localStorage.getItem("language"));
+    if (typeof window !== "undefined") {
+      const storedLang = localStorage.getItem("language");
+      if (storedLang && storedLang !== lang) {
+        dispatch(
+          storedLang === "ar" ? langAction.langAr() : langAction.langEn()
+        );
+      }
     }
-  }, []);
+  }, [dispatch, lang]);
 
   const messages = useMemo(() => {
     return lang === "ar" ? ar : en;
   }, [lang]);
+
+  return (
+    <IntlProvider
+      locale={lang}
+      messages={messages}
+      onError={(err) => {
+        if (err.code === "MISSING_TRANSLATION") {
+          console.warn("Missing translation:", err.message);
+          return;
+        }
+        throw err;
+      }}
+    >
+      <AuthInitializer>
+        {getLayout ? (
+          getLayout(<Component {...pageProps} locale={lang} />)
+        ) : (
+          <Layout>
+            <Component {...pageProps} locale={lang} />
+          </Layout>
+        )}
+        <BootStrapClient />
+      </AuthInitializer>
+    </IntlProvider>
+  );
+}
+
+export default function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <IntlProvider locale={lang} messages={messages} onError={() => null}>
-        <AuthInitializer>
-          {getLayout ? (
-            getLayout(<Component {...pageProps} />)
-          ) : (
-            <Layout>
-              <Component {...pageProps} locale={lang} />
-            </Layout>
-          )}
-          <BootStrapClient />
-        </AuthInitializer>
-      </IntlProvider>
+      <AppContent Component={Component} pageProps={pageProps} />
     </Provider>
   );
 }
