c6af5683a0d55aa73c0b5565538f45509b6f5e3b
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2025-05-18 22:05:52 +0300

    Update Title component

--- src/Components/SharedComponents/Title/Title.js
+++ src/Components/SharedComponents/Title/Title.js
@@ -1,7 +1,7 @@
 const Title = ({ text }) => {
   return (
-    <div className="tw-group">
-      <h2 className="tw-text-2xl sm:tw-text-3xl tw-font-bold tw-text-gray-800 tw-relative tw-inline-block">
+    <div className="tw-group tw-w-max">
+      <h2 className="tw-text-lg sm:tw-text-3xl tw-font-bold tw-text-gray-800 tw-relative tw-inline-block tw-whitespace-normal tw-break-words ">
         {text}
         <span className="tw-absolute tw-bottom-0 tw-left-0 tw-w-1/3 tw-h-1 tw-bg-gradient-to-r tw-from-[#44bcb7] tw-to-[#338e89] tw-transform tw-translate-y-1 group-hover:tw-w-full tw-transition-all tw-duration-300 tw-ease-in-out"></span>
       </h2>
