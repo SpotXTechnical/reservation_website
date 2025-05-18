fc83731fb361e125c4c5a310a6b8d107ed16d058
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2025-05-18 22:01:21 +0300

    Update property APIs

--- src/app/Apis/PropertyApis.jsx
+++ src/app/Apis/PropertyApis.jsx
@@ -37,3 +37,47 @@ export const getSummary = async (data) => {
     throw new Error(error.message);
   }
 };
+
+export const getPropertyReservationDetails = async (id) => {
+  try {
+    const response = await axiosInstance.get(
+      `/api/v1/user/units/${id}/reservations`
+    );
+    return response.data?.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const getPropertyAgeRanges = async (id) => {
+  try {
+    const response = await axiosInstance.get(
+      `/api/v1/user/units/${id}/age-ranges`
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const getPropertySupportedMeals = async (id) => {
+  try {
+    const response = await axiosInstance.get(
+      `/api/v1/user/units/${id}/supported-meals`
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const getHotelRoomReservationDetails = async (id, queryString) => {
+  try {
+    const response = await axiosInstance.get(
+      `/api/v1/user/units/${id}/reservations?${queryString}`
+    );
+    return response.data?.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
